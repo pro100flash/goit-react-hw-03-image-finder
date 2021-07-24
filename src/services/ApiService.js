ed4e8ca9244959cc631e1b1fa5788d03b1fce944
Yurii Khomin <yurii.khomin@gmail.com>
2021-07-24 21:31:07 +0300

    add common files

---
+++ src/services/ApiService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_KEY = "21713513-de4fa038d3971b80a05884d99";
+
+const apiService = ({ query = "", page = 1 }) => {
+  return axios
+    .get(
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+    .then(({ data }) => data.hits);
+};
+
+export default apiService;
