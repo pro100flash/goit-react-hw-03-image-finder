8ff7383a3cb1728f339328b42a296b15e18d5867
Yurii Khomin <yurii.khomin@gmail.com>
2021-07-25 06:30:31 +0300

    fix api

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -5,7 +5,7 @@ const API_KEY = "21713513-de4fa038d3971b80a05884d99";
 const apiService = ({ query = "", page = 1 }) => {
   return axios
     .get(
-      `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=15`
     )
     .then(({ data }) => data.hits);
 };
